da61a17ee9a3dd450e7a32dfd4b9ba24eb0fa7d9
sajim-khan <shossain1427@gmail.com>
2023-10-02 02:27:03 +0600

    added slider

---
+++ CareerBridge-client/src/Components/Slider/Slider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Slider = () => {
+    return (
+        <div>
+            <h1>This is slider component</h1>
+        </div>
+    );
+};
+
+export default Slider;
