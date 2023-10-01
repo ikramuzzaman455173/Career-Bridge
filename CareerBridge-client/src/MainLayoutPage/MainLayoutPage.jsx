7da3257a17ef0a993af8ed184c2ef4d488c05483
sajim-khan <shossain1427@gmail.com>
2023-10-02 02:06:28 +0600

    update layout page

--- CareerBridge-client/src/MainLayoutPage/MainLayoutPage.jsx
+++ CareerBridge-client/src/MainLayoutPage/MainLayoutPage.jsx
@@ -3,16 +3,16 @@ import { Outlet } from 'react-router-dom'
 import Navbar from '../Components/Navbar/Navbar'
 import Footer from '../Components/Footer/Footer'
 
-const MainLagoutPage = () => {
+const MainLayoutPage = () => {
   return (
-    <div className=''>
-      <Navbar/>
-      <div className='min-h-[calc(100vh-140px)]'>
-      <Outlet></Outlet>
+    <div className="">
+      <Navbar />
+      <div className="min-h-[calc(100vh-140px)]">
+        <Outlet></Outlet>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default MainLagoutPage
+export default MainLayoutPage;
