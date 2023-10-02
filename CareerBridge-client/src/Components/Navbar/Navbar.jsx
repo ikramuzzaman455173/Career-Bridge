a34f0b149d0cca97053413412d1ec155dfaa3ed6
sajim-khan <shossain1427@gmail.com>
2023-10-03 00:06:30 +0600

    update navbar menu option

--- CareerBridge-client/src/Components/Navbar/Navbar.jsx
+++ CareerBridge-client/src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-teal-100">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -27,51 +27,66 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a>Item 1</a>
+                <a>Home</a>
               </li>
               <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
+                <a>Job Poster</a>
               </li>
               <li>
-                <a>Item 3</a>
+                <a>Find Job</a>
+              </li>
+              <li>
+                <a>Blogs</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+              <li>
+                <a>FAQ</a>
+              </li>
+              <li>
+                <a>Contact</a>
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          {/* <a className="btn btn-ghost normal-case text-xl">daisyUI</a> */}
+          <img
+            className="rounded-full ms-2 lg:ms-12 my-1"
+            src="../../../public/logo.png"
+            alt="Career Logo"
+            width={130}
+            height={18}
+          />
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
+          <ul className="menu menu-horizontal bg-none px-5 text-lg ">
             <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
+              <a>Home</a>
             </li>
             <li>
-              <a>Item 3</a>
+              <a>Job Poster</a>
+            </li>
+            <li>
+              <a>Find Job</a>
+            </li>
+            <li>
+              <a>Blogs</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
+            <li>
+              <a>Contact</a>
             </li>
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <a className="btn btn-primary rounded-full px-6 me-3 lg:me-12">
+            Login
+          </a>
         </div>
       </div>
     </>
@@ -79,4 +94,3 @@ const Navbar = () => {
 }
 
 export default Navbar
-
